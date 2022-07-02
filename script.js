7c1ad9fe1743fbea10c2c14b9543461d9b198d28
Harim Tanko <harimtanko@gmail.com>
2022-07-02 12:45:26 +0100

    pipe collision code

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 import { updateBird, setupBird, getBirdRect } from './bird.js';
-import { updatePipes } from './pipe.js';
+import {
+  updatePipes,
+  setUpPipes,
+  getPassedPipeCount,
+  getPipeRects,
+} from './pipe.js';
 
 document.addEventListener('keypress', handleStart, { once: true });
 const title = document.querySelector('[data-title]');
@@ -22,12 +27,24 @@ function updateLoop(time) {
 
 function checkLose() {
   const birdRect = getBirdRect();
+  const insidePipe = getPipeRects().some((rect) => isCollision(birdRect, rect));
+
   const outsideWord = birdRect.top < 0 || birdRect.bottom > window.innerHeight;
-  return outsideWord;
+  return outsideWord || insidePipe;
+}
+
+function isCollision(rect1, rect2) {
+  return (
+    rect1.left < rect2.right &&
+    rect1.top < rect2.bottom &&
+    rect1.right > rect2.left &&
+    rect1.bottom > rect2.top
+  );
 }
 
 function handleStart() {
   setupBird();
+  setUpPipes();
   lastTime = null;
   title.classList.add('hide');
   window.requestAnimationFrame(updateLoop);
@@ -37,7 +54,7 @@ function handleLose() {
   setTimeout(() => {
     title.classList.remove('hide');
     subtitle.classList.remove('hide');
-    subtitle.textContent = '0 Pipes';
+    subtitle.textContent = `${getPassedPipeCount()} Pipes`;
     document.addEventListener('keypress', handleStart, { once: true });
   }, 100);
 }
